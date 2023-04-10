ef406f05b45c390732f13e077104640c213e230d
vamshiTanniru <119478238+vamshiTanniru@users.noreply.github.com>
2023-04-10 13:38:33 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,25 @@
+import streamlit as st
+
+st.title('Hello Guysss :wave: I am :blue[Vamshi Prakash Tanniru]')
+
+st.subheader("""I have complited my B.tech in (ECE) Engineering.
+                I am interested in problem-solving with an eye toward the fascinating world of Artificial Intelligence 
+              and I have a good understanding of Python . Being an AI Enthusiast :sunglasses:
+                I am here to learn and explore more in this domain!""")
+
+st.header("You can connect with me through :handshake: :")
+
+# x = st.slider('x')  # 👈 this is a widget
+# st.write(x, 'squared is', x * x)
+
+st.write(":safety_pin: [Github](https://github.com/vamshiTanniru)")
+
+st.write(":pushpin: [Linkedin](https://www.linkedin.com/in/vamshi-prakash/)")
+
+st.write(":e-mail: [Email](vamshiprakash001@gmail.com)")
+
+
+st.text("")
+st.text("")
+if st.button('Have a great day!'):
+    st.balloons()
